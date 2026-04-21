3ae0c81572282c6f7c564be22d0d3404362ac1da
Soumya Banerjee <neel.soumya@gmail.com>
2026-04-21 09:58:28 +0000

    edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.write(
 if "df" not in st.session_state:
 
     # Set seed for reproducibility.
-    np.random.seed(42)
+    np.random.seed(420)
 
     # Make up some fake issue descriptions.
     issue_descriptions = [
